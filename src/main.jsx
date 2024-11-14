8857fe9996627151000d6c2bb5bbb3a960c28ffd
adilansari03 <adilansari760725@gmail.com>
2024-11-14 15:05:01 +0530

    chnage routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { createRoot } from "react-dom/client";
+import React from "react";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+// import { HashRouter } from "react-router-dom";
 
+// createRoot(document.getElementById("root")).render(
+//   <HashRouter>
+//     <App />
+//   </HashRouter>
+// );
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>
+  </React.StrictMode>
 );
